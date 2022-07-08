fe09a1c20e89a7bb81669106a1ed7566f963f2a8
alan vergara <alan_vergara_p@hotmail.com>
2022-07-08 10:56:08 -0500

    solved on my own no help

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,2 +1,10 @@
 require_relative "coach_answer"
 # TODO: Implement the program that makes you discuss with your coach from the terminal.
+puts "---- Welcome to stupid coaching! ----"
+puts "----------------------------------- "
+input = nil
+until input == "I am going to work right now!"
+  puts "What is your message to the coach ?"
+  input = gets.chomp.to_s
+  puts coach_answer_enhanced(input)
+end
