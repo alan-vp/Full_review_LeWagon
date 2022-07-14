4dd7a51edb9b7e53ef87f8ce16f14c3ba8853f46
alan vergara <alan_vergara_p@hotmail.com>
2022-07-14 13:54:15 -0500

    solved but need more practice in for and while loops

--- 01-Ruby/02-Flow-Array/02-Simple-looping/lib/simple_looping.rb
+++ 01-Ruby/02-Flow-Array/02-Simple-looping/lib/simple_looping.rb
@@ -1,7 +1,25 @@
 def sum_with_for(min, max)
   # CONSTRAINT: you should use a for..end structure
+  return -1 if min > max
+
+  sum = 0
+  for number in min..max do
+    sum += number
+  end
+  return sum
 end
+# min = 1, max = 100 => sumar 1+2+3..100
 
 def sum_with_while(min, max)
   # CONSTRAINT: you should use a while..end structure
+  return -1 if min > max
+
+  counter = min
+  sum = 0
+
+  while counter <= max
+    sum += counter
+    counter += 1
+  end
+  sum
 end
